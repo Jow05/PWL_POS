b301595ff76d296cccc1357ff2ad622dab63034a
jow05 <ayujovita03@gmail.com>
2024-04-18 19:04:09 +0700

    Jobsheet 7: Prac-3

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\POSController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -52,5 +53,16 @@ Route::get('level/create', [LevelController::class, 'create'])->name('/level/cre
 Route::post('/level', [LevelController::class, 'store']);
 
 Route::resource('m_user', POSController::class);
-//Jobsheet 7
-Route::get('/', [WelcomeController::class, 'index' ]);
+
+Route::get('/', [WelcomeController:: class, 'index']);
+
+Route::group(['prefix' => 'user'], function(){
+    Route::get('/', [UserController::class, 'index']);          //menampilkan halaman awal user
+    Route::post('/list', [UserController::class, 'list']);      //menampilkan data user dalam bentuk json untuk datatables
+    Route::get('/create', [UserController::class, 'create']);   //menampilkan halaman form tambah user
+    Route::post('/', [UserController::class, 'store']);         //menyimpan data user baru
+    Route::get('/{id}', [UserController::class, 'show']);       //menampilkan detail user
+    Route::get('/{id}/edit', [UserController::class, 'edit']);  //menampilkan halam form edit user
+    Route::put('/{id}', [UserController::class, 'update']);     //menampilkan perubahan data user
+    Route::delete('/{id}', [UserController::class, 'destroy']); //menghapus data user
+});
